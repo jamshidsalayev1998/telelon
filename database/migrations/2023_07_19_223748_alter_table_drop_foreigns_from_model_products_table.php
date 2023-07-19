9d310014c1bd1c5868cb9aa6ff09d3e33ba4cbf2
= <=>
2023-07-19 22:39:02 +0500

    Create 2023_07_19_223748_alter_table_drop_foreigns_from_model_products_table.php

---
+++ database/migrations/2023_07_19_223748_alter_table_drop_foreigns_from_model_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableDropForeignsFromModelProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('model_products', function (Blueprint $table) {
+             $table->dropForeign('brand_id');
+             $table->dropForeign('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('model_products', function (Blueprint $table) {
+            //
+        });
+    }
+}
