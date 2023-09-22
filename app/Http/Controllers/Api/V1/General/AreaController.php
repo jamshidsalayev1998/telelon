eabc89cb23cce7bc69de0a668733ea5e18853a12
jamshidsalayev1998 <jmlaravel@mail.ru>
2023-09-22 09:53:39 +0500

    asd

--- app/Http/Controllers/Api/V1/General/AreaController.php
+++ app/Http/Controllers/Api/V1/General/AreaController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Api\V1\General;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\General\AreaResource;
 use App\Models\Area;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
+    use ApiResponser;
     public function index(Request $request)
     {
         $areas = Area::query()->with('translates')->filter($request->filters)->get();
-        return AreaResource::collection($areas);
+        $result['result'] = AreaResource::collection($areas);
+        return $this->success($result,'Success');
     }
 }
--- app/Http/Controllers/Api/V1/General/RegionController.php
+++ app/Http/Controllers/Api/V1/General/RegionController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Api\V1\General;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\General\RegionResource;
 use App\Models\Region;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class RegionController extends Controller
 {
+    use ApiResponser;
     public function index()
     {
         $regions = Region::with('translates')->get();
-        return RegionResource::collection($regions);
+        $result['result'] = RegionResource::collection($regions);
+        return $this->success($result,'Success');
     }
 }
